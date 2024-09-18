256231d7bc3c6a18c2c36179b1a79cfab6f49e8f
farhad_fbm <farhad.codes@gmail.com>
2024-09-18 08:27:34 +0600

    add 1

--- 1_array/1_maximum_minimum_element.cpp
+++ 1_array/1_maximum_minimum_element.cpp
@@ -2,6 +2,9 @@
 using namespace std;
 
 int main(){
-
+    vector<int> v ={ 1,2,3,4,2,9 };
+    sort(v.begin(), v.end());
+    cout<<"mini " << *v.begin()<<endl;
+    cout<<"maxi " << *(v.end()-1)<<endl;
     return 0;
 }
